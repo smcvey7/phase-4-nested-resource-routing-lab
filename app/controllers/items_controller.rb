86628175075b85a47143b77c6e7920e625822bdf
Steven McVey <smcvey1210@gmail.com>
2023-02-22 13:31:19 -0800

    commit tree 4475190456585a7d68ec6f067436c6fd81d393f5

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,34 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
 
-  def index
-    items = Item.all
-    render json: items, include: :user
+  def show
+    item = Item.find_by!(id: params[:id])
+    render json: item
   end
 
+  def index
+    if params[:user_id]
+      user = User.find_by!(id: params[:user_id])
+      render json: user.items, include: :user
+    else
+      items = Item.all
+      render json: items, include: :user
+    end
+
+    def create
+      item = Item.create(item_params)
+      render json: item, status: :created
+    end
+
+  end
+  private
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
+  end 
+
+  def render_record_not_found
+    # byebug
+    render json: {error: "User not found"}, status: :not_found
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
-
+rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
+  
   def show
-    user = User.find_by(id: params[:id])
+    user = User.find_by!(id: params[:id])
     render json: user, include: :items
   end
 
+  private
+  def render_record_not_found
+    # byebug
+    render json: {error: "User not found"}, status: :not_found
+  end
 end
